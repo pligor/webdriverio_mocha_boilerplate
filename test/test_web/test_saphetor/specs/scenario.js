5edfdd87934b7793c9c96f5b30582b8b18b27fbc
George Pligoropoulos <pligor.george@gmail.com>
2023-01-29 03:02:16 +0200

    created the first statement of the saphetor scenario

---
+++ test/test_web/test_saphetor/specs/scenario.js
@@ -0,0 +1,31 @@
+describe('Saphetor Test Challenge', () => {
+    it.only('Verify that ...........................................', async () => {
+        await browser.url(browser.options.baseUrl)
+
+        // const user = (await users)['valid']
+
+        // await LoginScreen.login(user)
+
+        // await step_assert_we_are_logged_in()
+
+        // await DashboardScreen.assert_is_rendered()
+
+        // console.log(`Verify that if a user is already logged in and attempts to navigate to the dashboard screen, then the user will be allowed to have access to the dashboard screen, namely that the login state is preserved`)
+        
+        // await DashboardScreen.access_for_logged_in_user()
+
+        // await DashboardScreen.assert_is_rendered()
+
+        // console.log('Verify that logging out of the Dashboard and attempting to access the Dashboard screen will redirect the user back to the login screen')
+
+        // await dashboard_header.logout()
+
+        // await DashboardScreen.access_for_logged_out_user()
+
+        // await LoginScreen.assert_login_is_reset()
+
+        // console.log('whatenver')
+
+        await browser.pause(3000)
+    })
+})
---
+++ test/test_web/test_saphetor/wdio.conf.saphetor.js
@@ -0,0 +1,27 @@
+const config = require('../../../wdio.conf').config
+
+const saphetor_config = {
+    //
+    // ==================
+    // Specify Test Files
+    // ==================
+    // Define which test specs should run. The pattern is relative to the directory
+    // of the configuration file being run.
+    //
+    // The specs are defined as an array of spec files (optionally using wildcards
+    // that will be expanded). The test for each spec file will be run in a separate
+    // worker process. In order to have a group of spec files run in the same worker
+    // process simply enclose them in an array within the specs array.
+    //
+    // If you are calling `wdio` from an NPM script (see https://docs.npmjs.com/cli/run-script),
+    // then the current working directory is where your `package.json` resides, so `wdio`
+    // will be called from there.
+    //
+    specs: [
+        // './test/test_web/test_saphetor/specs/**/*.js'
+        './specs/**/*.js'
+    ],
+    baseUrl: 'https://varsome.com',
+}
+
+exports.config = Object.assign({}, config, saphetor_config)
